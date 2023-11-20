4260c4677de6ce9407edf97c05d78b7c3cc4504b
TobyLyu <49470545+TobyLyu@users.noreply.github.com>
2023-11-20 19:16:22 +0800

    bug fix and update
    bug fix
    1. generating F matrix incorrectly
    2. scalar overflow due to incorrect nparray type
    3. bug in vec_sum
    
    update:
    1. add inverse matrix consistency check -> will output a warning if consistency not satisfied

--- core/src/GaloisField.py
+++ core/src/GaloisField.py
@@ -19,13 +19,13 @@ class GaloisField:
         elif self.w == 8:
             # x^8 + x^4 + x^3 + x^2 + 1
             self.prim_poly = 0b100011101
-            self.gflog = np.zeros(self.max_mask, dtype=np.uint8)
-            self.gfilog = np.zeros(self.max_mask, dtype=np.uint8)
+            self.gflog = np.zeros(self.max_mask, dtype=int)
+            self.gfilog = np.zeros(self.max_mask, dtype=int)
         elif self.w == 16:
             # x^16 + x^12 + x^3 + x + 1
             self.prim_poly = 0b10001000000001011
-            self.gflog = np.zeros(self.max_mask, dtype=np.uint16)
-            self.gfilog = np.zeros(self.max_mask, dtype=np.uint16)
+            self.gflog = np.zeros(self.max_mask, dtype=int)
+            self.gfilog = np.zeros(self.max_mask, dtype=int)
         self.gen_tables()
 
     def gen_tables(self):
@@ -74,8 +74,9 @@ class GaloisField:
         """
         if a == 0 or b == 0:
             return 0
-        # ipdb.set_trace()
+        
         log_sum = int((self.gflog[a] + self.gflog[b]) % (self.max_mask - 1)) # in case value overflow
+        # ipdb.set_trace()
         return int(self.gfilog[log_sum]) # lookup table method
     
     def division(self, a, b):
@@ -92,11 +93,30 @@ class GaloisField:
             return 0
         if b == 0:
             return -1
-        try:
-            log_minus = int((self.gflog[a] - self.gflog[b]) % (self.max_mask - 1)) # in case value overflow
-        except:
-            ipdb.set_trace()
+        # try:
+        # ipdb.set_trace()
+        log_minus = int((self.gflog[a] - self.gflog[b]) % (self.max_mask - 1)) # in case value overflow
+        # except:
+        #     ipdb.set_trace()
         return int(self.gfilog[log_minus])
+    
+    def pow(self, a, i):
+        """GF power
+
+        Args:
+            a (int): input power base
+            i (int): input power index
+
+        Returns:
+            int: output s_out
+        """
+        if a == 0 and i != 0:
+            return 0
+        
+        s_out = 1
+        for k in range(i):
+            s_out = self.multiply(s_out, a)
+        return s_out
     
     def vec_sum(self, v_a):
         """GF sum of vector
@@ -131,7 +151,7 @@ class GaloisField:
             v_b = np.array(v_b)
         log_sum = ((self.gflog[v_a] + self.gflog[v_b]) % (self.max_mask - 1)).astype(int)
         v_c = (self.gfilog[log_sum]).astype(int)
-        if any(v_a == 0) or any(v_b) == 0: # filter out zero multiply
+        if any(v_a == 0) or any(v_b == 0): # filter out zero multiply
             zero_idx = ~v_a.astype(bool) | ~v_b.astype(bool)
             v_c[zero_idx] = 0
         return np.bitwise_xor.reduce(v_c)
@@ -146,10 +166,12 @@ class GaloisField:
         Returns:
             nparray: NxK output m_c
         """
+        # ipdb.set_trace()
         assert m_a.shape[1] == m_b.shape[0]
         m_c = np.zeros([m_a.shape[0], m_b.shape[1]], dtype=int)
         for i in range(m_a.shape[0]):
             for j in range(m_b.shape[1]):
+                # ipdb.set_trace()
                 m_c[i, j] = self.vec_dot(m_a[i, :], m_b[:, j])
         return m_c
     
@@ -170,8 +192,18 @@ class GaloisField:
         m_i = np.eye(len_a, dtype=int)
         row_idx = list(range(len_a))
         for d_idx in row_idx: # now use the idx_th row
+            # print(m_a)
+            while m_a[d_idx, d_idx] == 0:
+                m_a = np.vstack([m_a, m_a[d_idx, :]])
+                m_a = np.delete(m_a, d_idx, 0)
+                m_i = np.vstack([m_i, m_i[d_idx, :]])
+                m_i = np.delete(m_i, d_idx, 0)
+            # print(m_a)
+            # ipdb.set_trace()
             div_ratio = m_a[d_idx, d_idx]
             for j in range(len_a): # scale this row
+                # print(m_a[d_idx, j], div_ratio)
+                # print(m_i[d_idx, j], div_ratio)
                 m_a[d_idx, j] = self.division(m_a[d_idx, j], div_ratio) 
                 m_i[d_idx, j] = self.division(m_i[d_idx, j], div_ratio) 
             for i in row_idx[:d_idx]+row_idx[d_idx+1:]: # scale other row
@@ -180,7 +212,9 @@ class GaloisField:
                     m_a[i, j] = self.sub(m_a[i, j], self.multiply(sub_ratio, m_a[d_idx, j])) 
                     m_i[i, j] = self.sub(m_i[i, j], self.multiply(sub_ratio, m_i[d_idx, j]))                   
             
-        # TODO: check inverse consistency: m_a * m_i = I
+        # check inverse consistency
+        if (self.matrix_multiply(m_i, i_a) != np.eye(len(m_a))).any():
+            print("Inverse result not consist. Input array may be singular!")
         return m_i   
             
 if __name__ == "__main__":
@@ -194,3 +228,10 @@ if __name__ == "__main__":
     print(gf.division(3, 7)) # =10
     print(gf.vec_sum([9, 11, 0, 0]))
     print(gf.vec_dot([3, 13, 0, 3], [7, 10, 7, 0])) # =[9, 11, 0, 0]
+    print(gf.matrix_inverse(np.array([[1, 0, 0], [1, 1, 1], [1, 2, 3]], dtype=int))) # [[1, 0, 0], [2, 3, 1], [3, 2, 1]]
+    
+    # gf = GaloisField(w = 8)
+    # print(gf.gflog)
+    # print(gf.gfilog)
+    # # print(gf.division(0, 4)) # =3
+    # print(gf.division(1, 4)) # =10
--- core/src/controller.py
+++ core/src/controller.py
@@ -5,22 +5,46 @@ from GaloisField import GaloisField
 
 class RAID6(GaloisField):
     def __init__(self, w, n, m) -> None:
+        """_summary_
+
+        Args:
+            w (int): word bit length
+            n (int): num of data disk
+            m (int): num of checksum disk
+        """
         super().__init__(w = w)
         self.disk_num = n
         self.check_num = m
         self.F = np.zeros([m, n], dtype=int)
         for i in range(0, m):
             for j in range(1, n+1):
-                self.F[i, j-1] = j ** i
+                self.F[i, j-1] = self.pow(j, i)
         self.A = np.vstack([np.eye(n, dtype=int), self.F])
     
     def encode(self, bit_str):
+        """encode the input bit string with checksum
+
+        Args:
+            bit_str (nparray): Nx1 input data
+
+        Returns:
+            nparray: encoded output with checksum
+        """
+        assert self.A.shape[1] == len(bit_str)
         return self.matrix_multiply(self.A, bit_str)
     
-    def decode(self, bit_str):
-        # ipdb.set_trace()
-        A_i = self.matrix_inverse(self.A[:self.disk_num, :])
-        return self.matrix_multiply(A_i, bit_str[:self.disk_num, :])
+    def decode(self, bit_str, use_disk):
+        """decode the input bit string from data or checksum
+
+        Args:
+            bit_str (Nx1): encoded input string
+            use_disk (list or nparray): 1xN list of disk available
+
+        Returns:
+            nparray: decoded output
+        """
+        A_i = self.matrix_inverse(self.A[use_disk, :])
+        return self.matrix_multiply(A_i, bit_str[use_disk, :])
     
 class Controller:
     def __init__(self) -> None:
@@ -35,9 +59,14 @@ class Controller:
     
 if __name__ == "__main__":
     # test case
+    rd6 = RAID6(w=4, n=6, m=2)
+    pw = rd6.encode(np.array([[13, 14, 15, 10, 11, 12]], dtype=int).T)
+    print(rd6.decode(pw, [0, 1, 2, 4, 5, 7]).T)
+    
     rd6 = RAID6(w=8, n=6, m=2)
-    # test_arr = np.array([[1, 0, 0], [1, 1, 1], [1, 2, 3]], dtype=int)
-    # result = rd6.matrix_inverse(test_arr) # [[1, 0, 0], [2, 3, 1], [3, 2, 1]]
     pw = rd6.encode(np.array([[32, 12, 24, 47, 10, 22]], dtype=int).T)
-    result = rd6.decode(pw)
-    print(result)
+    print(rd6.decode(pw, [0, 1, 2, 4, 5, 7]).T)
+    
+    rd6 = RAID6(w=16, n=8, m=2)
+    pw = rd6.encode(np.array([[543, 1231, 23425, 33, 765, 41435, 214, 27383]], dtype=int).T)
+    print(rd6.decode(pw, [0, 3, 4, 5, 6, 7, 8, 9]).T)
